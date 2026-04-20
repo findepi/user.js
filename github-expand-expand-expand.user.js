df501c5ab4c7424570dba3c35de1edb1b53e6a8e
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-04-20 10:18:24 +0200

    Update to current GitHub PR page

--- github-expand-expand-expand.user.js
+++ github-expand-expand-expand.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         GitHub PR: expand, expand, expand!
 // @namespace    http://findepi.github.io/
-// @version      0.5
-// @description  try to fix GitHub webapp annoyances introduced to fix GitHub webapp Unicorn!s on longer PRs
+// @version      0.6
+// @description  expand all github pull request review comments (workaround for https://github.com/isaacs/github/issues/1190)
 // @author       findepi
 // @license      MIT
 // @match        https://github.com/*/*/pull/*
@@ -12,13 +12,21 @@
 
 (function() {
     'use strict';
-
     console.log('Installing "Expand, expand"');
 
-    var box = jQuery(".pagehead-actions").first();
-
-    box.prepend('<li><a id="_f_expand_expand" class="btn btn-sm">Expand, expand!</a></li>');
-    jQuery('#_f_expand_expand').click(expandExpand);
+    var btn = $('<div>Expand, expand!</div>').css({
+        display: 'block',
+        position: 'fixed',
+        background: 'rgb(42, 49, 60)',
+        border: '1px dotted orange',
+        cursor: 'pointer',
+        padding: '3px',
+        top: '2px',
+        left: '2px',
+        zIndex: 9999
+    });
+    $('body').append(btn);
+    btn.on('click', expandExpand);
 
     function expandExpand() {
         var loadMore = jQuery('.ajax-pagination-btn:visible:contains(Load more)');
@@ -37,7 +45,8 @@
         }
 
         console.log('I don\'t see any "Load more" nor "Loading", so let\'s Expand, expand!');
-        jQuery('.show-outdated-button').click();
-        jQuery('.btn-link.Details-content--closed').click();
+        jQuery('review-thread-collapsible:not(.open)').each((_, el) => {
+            $(el).find('button').first().click();
+        })
     }
 })();
